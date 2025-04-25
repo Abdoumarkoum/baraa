32fdd1a51857ecadaf332a0cfe723c0da2170c26
Abdou.just.code <139796886+Abdoumarkoum@users.noreply.github.com>
2025-04-25 05:17:35 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+const restbnt = document.getElementById("restbnt");
+const increasebnt = document.getElementById("increasebnt");
+const countlabel = document.getElementById("countlabel");
+
+let count =0;
+
+increasebnt.onclick = function(){
+    count++;
+    countlabel.textContent = count;
+}
+
+
+
+restbnt.onclick = function(){
+    count = 0;
+    countlabel.textContent = count;
+}
+
+
+
+
+
+
+
+
+
+
+const restbnt1 = document.getElementById("restbnt1");
+const increasebnt1 = document.getElementById("increasebnt1");
+const countlabel1 = document.getElementById("countlabel1");
+
+let count1 =0;
+
+increasebnt1.onclick = function(){
+    count1++;
+    countlabel1.textContent = count1;
+}
+
+
+
+restbnt1.onclick = function(){
+    count1 = 0;
+    countlabel1.textContent = count1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const restbnt2 = document.getElementById("restbnt2");
+const increasebnt2 = document.getElementById("increasebnt2");
+const countlabel2 = document.getElementById("countlabel2");
+
+let count2 =0;
+
+increasebnt2.onclick = function(){
+    count2++;
+    countlabel2.textContent = count2;
+}
+
+
+
+restbnt2.onclick = function(){
+    count2 = 0;
+    countlabel2.textContent = count2;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const restbnt3 = document.getElementById("restbnt3");
+const increasebnt3 = document.getElementById("increasebnt3");
+const countlabel3 = document.getElementById("countlabel3");
+
+let count3 =0;
+
+increasebnt3.onclick = function(){
+    count3++;
+    countlabel3.textContent = count3;
+}
+
+
+
+restbnt3.onclick = function(){
+    count3 = 0;
+    countlabel3.textContent = count3;
+}
